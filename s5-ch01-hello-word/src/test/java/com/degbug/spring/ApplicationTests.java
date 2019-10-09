28249931379ed6c6e1a8df2c54f4b9a27e696a0e
jianhualei <jianhua.lei@ximalaya.com>
2019-10-09 10:58:42 +0800

    测试代码增加注释

--- s5-ch01-hello-word/src/test/java/com/degbug/spring/ApplicationTests.java
+++ s5-ch01-hello-word/src/test/java/com/degbug/spring/ApplicationTests.java
@@ -3,7 +3,6 @@ package com.degbug.spring;
 import com.degbug.spring.hello.Application;
 import com.degbug.spring.hello.MessagePrinter;
 import org.junit.Test;
-import static org.junit.Assert.*;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -15,7 +14,9 @@ public class ApplicationTests {
     @Test
     public void main(){
         ApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
+        //获取bean
         MessagePrinter messagePrinter = context.getBean(MessagePrinter.class);
+        //输出Hello World
         messagePrinter.printMessage();
     }
 }
